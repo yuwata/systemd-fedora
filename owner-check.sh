a004447ba3bbee66ec8925dd756af8f8335c792e
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2021-03-30 12:10:33 +0200

    owner-check: make sending of mails optional

--- owner-check.sh
+++ owner-check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-[ -z "$server" -o -z "login" ] && { echo '$server and $login need to be set'; exit 1 }
+verb="$1"
+
+[ "$verb" = "-s" ] && do_send=1 || do_send=
+
+[ -n "$do_send" ] && [ -z "$server" -o -z "login" ] && { echo '$server and $login need to be set'; exit 1; }
 
 header=
 from=systemd-maint@fedoraproject.org
@@ -11,11 +15,22 @@ port=587
 
 for user in "$@"; do
     echo "checking $user…"
-    t=$(git shortlog --all --author $user --since "@{$time}" | wc -l)
+
+    p=$(git log -1 --all --author "$user")
+    if [ -z "$p" ]; then
+	echo "No commits from $user, check spelling"
+	exit 1
+    fi
+
+    t=$(git shortlog --all --author "$user" --since "@{$time}" | wc -l)
     if [ $t != 0 ]; then
 	echo "$t commits in the last two years, OK"
+	echo
 	continue
     fi
+
+    echo "$p" | head -n6
+    echo ".. adding to list"
 
     if [ -z "$header" ]; then
 	echo '$USER$;$EMAIL$' >.mail.list
@@ -23,14 +38,16 @@ for user in "$@"; do
     fi
 
     echo "$user;$user@fedoraproject.org" >>.mail.list
+    echo
 done
 
 [ -z "$header" ] && exit 0
+[ -n "$do_send" ] || exit 0
 
 echo "Sending mails…"
 set -x
-massmail -F $from \
-	 -C $from \
+massmail -F "$from" \
+	 -C "$from" \
 	 -S 'write access to the fedora systemd package' \
-	 -z $server -u $login -P $port \
+	 -z "$server" -u "$login" -P "$port" \
 	 .mail.list <owner-check.template
