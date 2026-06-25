9d4edaa576e609413cdc987e6127e5f228c62ac6
Frantisek Sumsal <frantisek@sumsal.cz>
2026-06-25 16:04:01 +0200

    test: work around a kernel bug in virtio/vsock
    There's a virtio/vsock bug in kernel 7.1-rc where a patch for a
    potential overflow inadvertently shrunk the receive buffer's effective
    size below what was configured, which eventually causes the vsock
    connection to get reset with ENOBUFS, that kills the journal forwarding
    over vsock:
    
    read(7, ..., 16392): No buffer space available
    Entry too big, skipped
    read(8, ..., 16392): No buffer space available
    Entry too big, skipped
    read(12, ..., 16392): No buffer space available
    Entry too big, skipped
    Finishing after writing 6685 entries
    
    This then affects other systemd components that try to write to the now
    broken journal, causing them to fail as well.
    
    Let's just bump the default vsock buffer size to work around this until
    the kernel fix lands.
    
    Pending fix: https://lore.kernel.org/netdev/20260518090656.134588-3-sgarzare@redhat.com/
    
    [skip changelog]

--- plans/run-integration-tests.sh
+++ plans/run-integration-tests.sh
@@ -94,6 +94,14 @@ if [[ "$NPROC" -gt 4 ]]; then
     NPROC=4
 fi
 
+# Workaround for a kernel 7.x virtio/vsock bug, where a patch for a potential overflow inadvertently shrunk
+# the receive buffer's effective size below what was configured, which eventually causes the vsock connection
+# to get reset with ENOBUFS, that kills the journal forwarding over vsock
+#
+# Pending fix: https://lore.kernel.org/netdev/20260518090656.134588-3-sgarzare@redhat.com/
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+
 # This test is only really useful if we're building with sanitizers and takes a long time, so let's skip it
 # for now.
 export TEST_SKIP="TEST-21-DFUZZER ${TEST_SKIP:-}"
